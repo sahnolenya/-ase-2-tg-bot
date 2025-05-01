c575a84caca8f31c96448c5ce9367c91d8f91154
sahnolenya <lyonya.saxno@mail.ru>
2025-05-01 18:38:17 +0300

    Подправил тест запуска бота и fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,12 +1,7 @@
-
 import pytest
 from unittest.mock import AsyncMock, patch
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram import Router
-
-# Фикстуры в pytest позволяют выносить в отдельные функции типовые действия
-# например: настройка тестового окружения, создание тестовых данных, выполнение завершающие действия
-# https://habr.com/ru/articles/731296/
 
 @pytest.fixture
 def mock_bot():
@@ -17,25 +12,20 @@ def mock_bot():
         mock_bot_cls.return_value = mock_bot_instance
         yield mock_bot_instance
 
-
 @pytest.fixture()
 def mock_set_my_commands():
-    """Моск создание меню"""
-    with patch(target: "main.set_my_commands", new_callable=AsyncMock) as mock:
-    yield mock_set_my_commands
+    """Mock создание меню"""
+    with patch("main.set_my_commands", new_callable=AsyncMock) as mock:
+        yield mock
 
 @pytest.fixture
 def mock_dispatcher():
-    """Моск диспетчер"""
-    with patch("main. Dispatcher") as mock_dispatcher_cls:
+    """Mock диспетчер"""
+    with patch("main.Dispatcher") as mock_dispatcher_cls:
         mock_dispatcher_instance = AsyncMock()
         mock_dispatcher_instance.start_polling = AsyncMock()
         mock_dispatcher_cls.return_value = mock_dispatcher_instance
         yield mock_dispatcher_instance
-
-
-
-
 
 @pytest.fixture
 def mock_message():
@@ -43,9 +33,22 @@ def mock_message():
     mock_msg = AsyncMock(spec=Message)
     mock_msg.answer = AsyncMock()
     mock_msg.from_user = AsyncMock()
-    mock_msg.from_user.id = AsyncMock()
-    mock_msg.from_user.username = AsyncMock()
+    mock_msg.from_user.id = 123
+    mock_msg.from_user.username = "test_user"
+    mock_msg.text = ""
     return mock_msg
+
+@pytest.fixture
+def mock_callback():
+    """Mock коллбэк"""
+    mock_cb = AsyncMock(spec=CallbackQuery)
+    mock_cb.message = AsyncMock(spec=Message)
+    mock_cb.message.answer = AsyncMock()
+    mock_cb.data = ""
+    mock_cb.from_user = AsyncMock()
+    mock_cb.from_user.id = 123
+    mock_cb.answer = AsyncMock()
+    return mock_cb
 
 @pytest.fixture
 def mock_router():
